f97c2a1c0784a9c04860570c2da99ef6f458b988
Willian Robson <120076321+williansnex@users.noreply.github.com>
2023-09-10 19:46:21 -0300

    logo + shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+import logomeme from '@/image/logo.svg'
+import { Card } from '@/components/ui/card'
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="w-full h-full flex items-center justify-center flex-col">
+      <Image src={logomeme} alt="mememaker" className="mx-auto" />
+      <Card>
+        <h1>Hello World</h1>
+      </Card>
+    </div>
   )
 }
